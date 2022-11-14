8e3721d240e5e21f0f944d4e0d36ea9f1313e6c9
aekala <lkkodish@gmail.com>
2022-11-14 13:32:53 -0800

    homepage

---
+++ script.js
@@ -0,0 +1,31 @@
+function getDayOfWeek(date) {
+    return date.toLocaleDateString('en-US', { weekday: 'long' })
+}
+
+function getTimeScopedWelcome(date) {
+    const militaryHour = date.getHours();
+    let welcome = "";
+    switch (true) {
+        case (militaryHour < 4): 
+            welcome = "good night";
+            break;
+        case (militaryHour < 12):
+            welcome = "good morning";
+            break;
+        case (militaryHour < 18):
+            welcome = "good afternoon";
+            break;
+        case (militaryHour < 22):
+            welcome = "good evening";
+            break;
+        default: 
+            welcome = "hi";
+    }
+    
+    return welcome;
+}
+
+let greetingObj = document.getElementById('Greeting');
+const date = new Date();
+let message = `today is <b> ${getDayOfWeek(date).toLowerCase()}, ${getTimeScopedWelcome(date)} </b> aekala!`;
+greetingObj.innerHTML = message;
